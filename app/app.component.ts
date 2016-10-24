dce7e14bde5e20683730ccb958da8be22fc4772e
Corneezius <cfr2116@columbia.edu>
2016-10-23 23:48:04 -0400

    update App Component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>My First 2 App </h1>
+    <h1>Meal Tracker</h1>
+    <meal-list
+      [childTaskList]="masterMealList"
+      (clickSender)="showDetails($event)"
+     ></meal-list>
+    <edit-meal
+      [childSelectedTask]="selectedMeal"
+      (doneClickedSender)="finishedEditing()"
+    ></edit-meal>
+    <new-meal
+    (newMealSender)="addMeal($event)"
+  ></new-meal>
   </div>
   `
 })
 
 export class AppComponent {
-
+  public masterMealList: Meal[] = [
+      new Meal("Chicken and Rice","Chicken and Rice", 350,  0),
+      new Meal("Chicken and Rice","Chicken and Rice", 350,  0),
+      new Meal("Chicken and Rice","Chicken and Rice", 350,  0),
+      new Meal("Chicken and Rice","Chicken and Rice", 350,  0),
+  ];
+  selectedMeal: Meal = null;
+  showDetails(clickedMeal: Meal) {
+    this.selectedMeal = clickedMeal;
+  }
+  finishedEditing() {
+    this.selectedMeal = null;
+  }
+  addMeal(newMealFromChild: Meal) {
+   this.masterMealList.push(newMealFromChild);
+ }
 }
